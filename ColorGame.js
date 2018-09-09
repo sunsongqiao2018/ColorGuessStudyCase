2ae65a791808da8c9c9945f6fecfb4b4ee094a16
Joel2018 <sunsongqiao2015@gmail.com>
2018-09-09 11:19:53 -0400

    Click Event updates
    Added click event for squares to give you a feedback when you interact with them

--- ColorGame.js
+++ ColorGame.js
@@ -13,10 +13,24 @@ var squares = document.querySelectorAll(".square");
 
 var pickedColor = colors[4];
 
-var goalcolor = document.getElementById("goal");
-goalcolor.textContent= pickedColor;
+var goalcolorSetter = document.getElementById("goal");
+goalcolorSetter.textContent= pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
-
+	//add initial colors for squares
 	squares[i].style.backgroundColor = colors[i];
+
+	//add click events for squares
+	squares[i].addEventListener("click",function()
+	{
+		var clickColor = this.style.backgroundColor;
+
+	if(pickedColor === clickColor)
+	{
+		alert("right!");
+	}else
+	{
+		alert("wrong!");
+	}
+	});
 }
