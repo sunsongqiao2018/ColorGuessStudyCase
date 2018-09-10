17a75ab587de2f906d1b6cc8788e2572f8149b68
Joel2018 <sunsongqiao2015@gmail.com>
2018-09-09 21:12:16 -0400

    Optimized Random features
    This update includes the added feature of generate 6 different rand-color after every refreash. And the target color is also randomly selected among those six colors.

--- ColorGame.js
+++ ColorGame.js
@@ -1,20 +1,16 @@
 //alert("connected");
 
-var colors = [
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(255, 0, 255)",
-"rgb(0, 0, 0)",
-"rgb(0, 255, 255)",
-"rgb(255, 255, 255)"
-]
+var colors = randomColorGenerator(6);
 
 var squares = document.querySelectorAll(".square");
-
-var pickedColor = colors[4];
-
+var pickedColor = pickColor();
 var goalcolorSetter = document.getElementById("goal");
-goalcolorSetter.textContent= pickedColor;
+var messageBar = document.getElementById("message");
+var header = document.querySelector("h1");
+
+goalcolorSetter.textContent = pickedColor;
+
+
 
 for (var i = 0; i < squares.length; i++) {
 	//add initial colors for squares
@@ -27,10 +23,83 @@ for (var i = 0; i < squares.length; i++) {
 
 	if(pickedColor === clickColor)
 	{
-		alert("right!");
-	}else
+		//get the right color
+		messageBar.textContent = "right!";
+		changeColor(pickedColor);
+
+	}
+	else
 	{
-		alert("wrong!");
+		//alert("wrong!");
+		this.style.backgroundColor = "#232323";
+		messageBar.textContent = "try again";
 	}
 	});
+}
+
+function changeColor(getpickedColor)
+{
+	//instead of change the color square get, we can change the color directly.
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = getpickedColor;
+	}
+	header.style.backgroundColor = getpickedColor;
+
+}
+
+function pickColor()
+{
+	var randomNum = Math.floor(Math.random() * colors.length);
+	return colors[randomNum];
+}
+
+function randomColorGenerator(num)
+{
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		arr.push(randomColor());
+	}
+
+	return arr
+}
+
+function randomColor()
+{
+	//generate red;
+	var red = Math.floor(Math.random()*256);
+	//generate green;
+	var green = Math.floor(Math.random()*256);
+
+	//generate blue
+	var blue = Math.floor(Math.random()*256);
+	var colorCode = "rgb(" +red+ ", " +green+ ", " +blue+ ")";
+	return colorCode
+}
+
+function choiceLevel()
+{
+	var hardVersion = false;
+	var easyVersion = false;
+	var hardLevel = document.getElementById("hard");
+
+	hardLevel.addEventListener("click",function(){
+		hardVersion = true;
+	})
+
+	var easyLevel = document.getElementById("easy");
+	
+	esayLevel.addEventListener("click",function()
+	{
+		easyVersion = true;
+	})
+	console.log(hardVersion);
+	if(hardVersion){
+		console.log("actived")
+		return 6;
+	}
+	if(easyVersion)
+	{
+		return 3;
+	}
+
 }
